fd4ac93c24b540270cdc24e1bab19d9feac7a014
shg1969 <shg1969@outlook.com>
2023-11-11 18:33:44 +0800

    Create win_create.h

---
+++ win_create.h
@@ -0,0 +1,16 @@
+#ifndef WIN_CREATE_H
+#define WIN_CREATE_H
+
+#include <QWidget>
+
+class Win_Create : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Win_Create(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // WIN_CREATE_H
