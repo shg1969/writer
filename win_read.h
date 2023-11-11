746a010d1fcdca71ec2f576f70f510f93ee0f188
shg1969 <shg1969@outlook.com>
2023-11-11 18:32:33 +0800

    Update win_read.h

--- win_read.h
+++ win_read.h
@@ -9,21 +9,43 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QFormLayout>
-//#include <QHBoxLayout>
+#include <QListWidget>
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QListWidgetItem>
+#include <QTextBrowser>
+#include <QHash>
+#include <QString>
+#include <QVector>
 
-class Win_Read : public QWidget
+class Win_Read : public QDockWidget
 {
     Q_OBJECT
 public:
     explicit Win_Read(QWidget *parent = nullptr);
+public slots:
+    void auto_set_key_focus(QString content);
 private:
-    QDockWidget *dock_main;
+    QString selected_in_txt;
+    QHash<QString,QVector<QString>> lib_recent_added;
+    QGroupBox *all;
     QVBoxLayout *layout_main;
-    QLabel *key;
+
+    QHBoxLayout *layout_key;
+    QCheckBox *checkBox_auto_focus;
+    QLabel *label_key;
     QLineEdit *edit_key;
 
+    QHBoxLayout *layout_recent;
+    QLabel *label_recent_added;
+    QPushButton *clear_recent_record;
+    QListWidget *listView_recent_added;
+    QLabel *label_content;
+    QTextBrowser *content_browse;
 
 signals:
+private slots:
+    void show_select_item(QListWidgetItem *item);
 
 };
 
