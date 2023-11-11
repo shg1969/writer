21b2f22d3182a6aaea0d47573d5e359f734c704d
shg1969 <shg1969@outlook.com>
2023-11-11 18:32:47 +0800

    Update win_main.h

--- win_main.h
+++ win_main.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 #include "win_bookshelf.h"
 #include "win_preview.h"
+#include "win_read.h"
 #include "setting.h"
 #include <QMainWindow>
 #include<QPlainTextEdit>
@@ -13,11 +14,14 @@ class Win_Main : public QMainWindow
 public:
     Win_Main(QWidget *parent = nullptr);
     ~Win_Main();
+signals:
+    void txt_selection_Changed(QString);
 
 private:
-    QPlainTextEdit *edit_win;
+    QPlainTextEdit *edit_txt;
     Win_Preview *preview_win;
     Win_Bookshelf *bookshelf_win;
+    Win_Read*record_win;
     Setting setting;
     Book book;
 
