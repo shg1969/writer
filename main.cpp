f56b50c5586950a4d6b4474c245e0b54362e8903
shg1969 <shg1969@outlook.com>
2023-11-11 10:04:37 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#define __TEST
+#ifdef __TEST
+#include "preview_txt.h"
+#include "bookshelf.h"
+#include "setting.h"
+#include "book.h"
+#include<QDebug>
+#endif
+
+#ifdef __TEST
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    return a.exec();
+}
+#endif
+
+#ifndef __TEST
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+#endif
