d4ff5d1e876141189fb94a59f1c33552a053da0f
shg1969 <shg1969@outlook.com>
2023-11-11 12:15:33 +0800

    Create option.h

---
+++ option.h
@@ -0,0 +1,49 @@
+#ifndef OPTION_H
+#define OPTION_H
+
+#include <QDialog>
+#include <QString>
+#include <QVector>
+#include <QFile>
+#include <QDialog>
+#include<QPushButton>
+#include<QLabel>
+#include<QComboBox>
+#include<QLineEdit>
+#include<QFormLayout>
+#include<QVBoxLayout>
+#include<QHBoxLayout>
+#include<QTextCodec>
+
+#include "setting.h"
+#include <QTextStream>
+
+class Option : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit Option(Setting *set,QDialog *parent = nullptr);
+private:
+    QVBoxLayout *main_layout;
+    QFormLayout *set_item_layout;
+    QHBoxLayout *H_layout_1;
+    QHBoxLayout *H_layout_2;
+    QHBoxLayout *btn_layout;
+
+    QPushButton *browse_shelf_dir;
+    QPushButton *browse_database_dir;
+    QComboBox *coding_type;
+
+    QLineEdit *edit_shelf_dir;
+    QLineEdit *edit_database_dir;
+
+    QPushButton *ok;
+    QPushButton *cancel;
+
+signals:
+
+
+};
+
+
+#endif // OPTION_H
