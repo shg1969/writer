4a1aded7fba7a95179e0a0c4dd2891b2d7f92281
shg1969 <shg1969@outlook.com>
2023-11-12 11:07:50 +0800

    Update win_preview.h

--- win_preview.h
+++ win_preview.h
@@ -18,9 +18,11 @@ public:
     //提供预览窗口
     explicit Win_Preview(QWidget *parent);
     void preview_show(QString book_name,QByteArray txt);
+    ~Win_Preview();
 signals:
     void sent_encoding_type(QString new_coding);
 private:
+    QVBoxLayout *main_layout;
     QTextBrowser *browser;
     QComboBox *ComboBox_celect_coding;
     QPushButton *ok;
@@ -28,6 +30,7 @@ private:
     QLabel *label_encoding;
     QLabel *label_book_name;
     QHBoxLayout *layout_set_encoding;
+    QHBoxLayout *btn_layout;
 
     QString encoding_type;//编码方式名称
     QByteArray preview_txt;//预览文段
