7388211e1f83f46d53cb56cf7c68e24208539b30
shg1969 <shg1969@outlook.com>
2023-11-12 11:07:07 +0800

    Update win_directory.cpp

--- win_directory.cpp
+++ win_directory.cpp
@@ -16,6 +16,14 @@ Win_Directory::Win_Directory(QStringList directory,QWidget *parent) : QDockWidge
         QListWidgetItem *a=new QListWidgetItem(i,listView_directory);
         listView_directory->scrollToItem(a);
     }
+
+    //***************信号和槽******************
+    connect(listView_directory,&QListWidget::itemClicked,this,&Win_Directory::select_directory);
+}
+
+void Win_Directory::select_directory(QListWidgetItem *item)
+{
+    emit click_on_directory(item->text());
 }
 
 
